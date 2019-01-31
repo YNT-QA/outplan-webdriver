e432395a782ef4c5f1808646e07b2ccc9a5875fe
gurongrong0329 <648186030@qq.com>
2019-01-31 21:10:33 +0800

    update

---
+++ src/pages/scene_page.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+# 作者: 顾名思义
+# 时间: 2019/1/29 14:50
+# 文件: scene_page.py
+import sys
+sys.path.append('..')
+from common.get_value import GetValue
+from time import sleep
+
+class ScenePage():
+
+    def __init__(self,driver):
+        self.driver = driver
+        self.data = GetValue()
+
+    #进入话述配置菜单
+    def into_scene(self):
+        self.driver.find_element_by_xpath(self.data.getvalue('e_intoScene')).click()
+        sleep(2)
+
+    #创建场景库
+    def create_scene(self,sceneName):
+        self.driver.find_element_by_xpath(self.data.getvalue('e_sceneManage')).click()
+        sleep(2)
+        self.driver.find_element_by_xpath(self.data.getvalue('e_textVersion')).click()
+        sleep(2)
+        self.driver.find_element_by_xpath(self.data.getvalue('e_createScene')).click()
+        sleep(1)
+
+        elements=self.driver.find_elements_by_xpath(self.data.getvalue('e_scene_Name'))
+        for element in elements:
+            try:
+                element.send_keys(sceneName)
+            except:
+                pass
+        sleep(1)
+
+        self.driver.find_element_by_xpath(self.data.getvalue('e_businessType')).click()
+        sleep(2)
+
+        elements3 =self.driver.find_elements_by_xpath(self.data.getvalue('e_type_financial'))
+        for element in elements3:
+            try:
+                element.click()
+            except:
+                pass
+        sleep(2)
+
+        elements4 =self.driver.find_elements_by_xpath(self.data.getvalue('e_loans'))
+        for element in elements4:
+            try:
+                element.click()
+            except:
+                pass
+        sleep(1)
+
+        elements2=self.driver.find_elements_by_xpath(self.data.getvalue('e_ensure'))
+        for element in elements2:
+            try:
+                element.click()
+            except:
+                pass
+
+        flag=True
+        while flag:
+            try:
+                self.driver.find_element_by_xpath(self.data.getvalue('e_deleteScene').replace('%var%',sceneName))
+                flag=False
+            except:
+                pass
+
+    #删除场景库
+    def delete_scene(self,sceneName):
+        sleep(2)
+        self.driver.find_element_by_xpath(self.data.getvalue('e_deleteScene').replace('%var%',sceneName)).click()
+        sleep(1)
+        self.driver.find_element_by_xpath(self.data.getvalue('e_endEnsure')).click()
+        sleep(2)
---
+++ src/testCases/test_createscene.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# 作者: 顾名思义
+# 时间: 2019/1/29 17:03
+# 文件: test_createscene.py
+import unittest
+import sys
+sys.path.append('..')
+from selenium import webdriver
+from common.get_path import GetPath
+from common.get_value import GetValue
+from pages.login import Login
+from common.assertFunction import AssertFunction
+from pages.scene_page import ScenePage
+
+class TestCreateScene(unittest.TestCase):
+    driver = None
+    data = None
+    plan = None
+    ft = None
+    user=None
+    scene=None
+
+    def setUp(self):
+        global driver,data,ft,user
+        data = GetValue()
+        browser = GetPath(data.getvalue('driver'))
+        driver = webdriver.Chrome(browser.get_filePath())
+        # 登录
+        user = Login(driver)
+        user.login(data.getvalue('address'), data.getvalue('account'), data.getvalue('password'))
+        ft = AssertFunction()
+        self.assertTrue(ft.isElementExist(driver, 'e_personalDetails'))
+
+    def test_create_scene(self):
+        u"""创建场景库"""
+        global scene
+        scene=ScenePage(driver)
+        scene.into_scene()
+        scene.create_scene(data.getvalue('groupName'))
+
+
+    def tearDown(self):
+        scene.delete_scene(data.getvalue('groupName'))
+        # 登出
+        user.loginOut(data.getvalue('account'))
+        driver.quit()
